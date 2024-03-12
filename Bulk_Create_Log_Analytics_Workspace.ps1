72f28e60a4399392b1abe8c3a3afba227cbb6983
ashraf2944 <47142914+ashraf2944@users.noreply.github.com>
2024-03-12 18:18:52 +0530

    Add files via upload

---
+++ Bulk_Create_Log_Analytics_Workspace.ps1
@@ -0,0 +1,34 @@
+﻿Connect-AzAccount
+
+
+# Read data from Excel sheet (replace file path and sheet name)
+$ExcelFile = "C:\Excel_file_path.xlsx"
+$ExcelData = Import-Excel -Path $ExcelFile -WorksheetName "Your-Worksheet-Name" | Select-Object "ResourceGroupName", "Location", "SubscriptionId", "LogAnalyticsWorkspaceName"
+
+
+
+# Iterate through each row and create workspace
+foreach ($row in $ExcelData) {
+    $ResourceGroupName = $row.ResourceGroupName
+    $Location = $row.Location
+    $SubscriptionId = $row.SubscriptionId
+    $LogAnalyticsWorkspaceName = $row.LogAnalyticsWorkspaceName
+Set-AzContext -Subscription $SubscriptionId
+
+$resourcegrouptags = Get-AzResourceGroup -Name $ResourceGroupName
+ 
+# Initialize an empty hashtable
+$tagHashtable = @{}
+
+# Extract the tags and their values
+foreach ($tag in $resourcegrouptags.Tags.GetEnumerator()) {
+$tagHashtable[$tag.Name] = $tag.Value
+}
+ # Create workspace
+New-AzOperationalInsightsWorkspace -ResourceGroupName $ResourceGroupName -Location $Location -Name $LogAnalyticsWorkspaceName -Tag $tagHashtable
+
+Write-Host "Workspace '$LogAnalyticsWorkspaceName' created successfully."
+
+}
+
+Write-Host "Script execution completed."
